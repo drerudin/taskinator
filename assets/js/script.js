2402ea7c8266b2b590f21b96744f547587a6ca6e
Andrey Rudinskiy <drerudin@gmail.com>
2022-04-28 22:48:18 -0700

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var createTaskHandler = function() {
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 buttonEl.addEventListener("click", createTaskHandler);
+
+
